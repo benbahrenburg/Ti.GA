c35d321e6d8c1ec964eb212481e7270a13f0dd55
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2014-02-20 11:28:47 -0800

    Fix Google Analytics AdSupport Framework dependency

--- iOS/build/googleanalytics.build/Release-iphoneos/googleanalytics.build/Objects-normal/armv7s/TiGoogleanalyticsModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/benjamin/Projects/Modules/TiGA/ios/Classes/TiGoogleanalyticsModuleAssets.m \
-  /Users/benjamin/Projects/Modules/TiGA/ios/Classes/TiGoogleanalyticsModuleAssets.h
--- iOS/build/googleanalytics.build/Release-iphonesimulator/googleanalytics.build/Objects-normal/i386/TiGoogleanalyticsFields.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/benjamin/Projects/Modules/TiGA/ios/Classes/TiGoogleanalyticsFields.m \
-  /Users/benjamin/Projects/Modules/TiGA/ios/Classes/TiGoogleanalyticsFields.h \
-  /Users/benjamin/Projects/Modules/TiGA/ios/Library/GAIFields.h
