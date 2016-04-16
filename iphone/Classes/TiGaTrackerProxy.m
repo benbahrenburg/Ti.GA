6b8a4ffa9abacfdffe7e5ad8b7658e3824e96450
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2016-04-16 10:40:20 -0700

    update iOS google analytics

--- iphone/Classes/TiGaTrackerProxy.m
+++ iphone/Classes/TiGaTrackerProxy.m
@@ -57,7 +57,7 @@
     if(_debug){
         NSLog(@"[DEBUG] Starting Session");
     }
-    [_tracker send:[[[GAIDictionaryBuilder createAppView] set:@"start" forKey:kGAISessionControl] build] ];
+    [_tracker send:[[[GAIDictionaryBuilder createScreenView] set:@"start" forKey:kGAISessionControl] build] ];
 }
 
 -(void)endSession:(id)unused
@@ -66,7 +66,7 @@
     if(_debug){
         NSLog(@"[DEBUG] Ending Session");
     }
-    [_tracker send:[[[GAIDictionaryBuilder createAppView] set:@"end" forKey:kGAISessionControl] build] ];
+    [_tracker send:[[[GAIDictionaryBuilder createScreenView] set:@"end" forKey:kGAISessionControl] build] ];
 }
 
 -(void)addScreenView:(id)args
@@ -78,7 +78,7 @@
         NSLog(@"[DEBUG] addScreenView: %@", screen);
     }
     [_tracker set:kGAIScreenName value:screen];
-    [_tracker send:[[GAIDictionaryBuilder createAppView] build]];
+    [_tracker send:[[GAIDictionaryBuilder createScreenView] build]];
 }
 
 -(void)addEvent:(id)args
